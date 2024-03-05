05336e63cb3d81fa5a0bf996131ee153bcb8cb0e
quangnmwork <quangnm.work@gmail.com>
2024-03-05 13:20:58 +0900

    fix: all eslint

--- frontend/src/feature/Auth/LoginForm/LoginForm.stories.tsx
+++ frontend/src/feature/Auth/LoginForm/LoginForm.stories.tsx
@@ -1,7 +1,8 @@
 import { LoginForm } from './LoginForm';
-import type { Meta, StoryObj } from '@storybook/react';
 
-const meta: Meta<typeof LoginForm> = { component: LoginForm, title: "Features/LoginForm" };
+import type { Meta } from '@storybook/react';
+
+const meta: Meta<typeof LoginForm> = { component: LoginForm, title: 'Features/LoginForm' };
 
 export default meta;
 export const Overview = { args: {} };
--- frontend/src/feature/Auth/RegisterForm/RegisterForm.stories.tsx
+++ frontend/src/feature/Auth/RegisterForm/RegisterForm.stories.tsx
@@ -1,7 +1,8 @@
 import { RegisterForm } from './RegisterForm';
-import type { Meta, StoryObj } from '@storybook/react';
 
-const meta: Meta<typeof RegisterForm> = { component: RegisterForm, title: "Features/RegisterForm" };
+import type { Meta } from '@storybook/react';
+
+const meta: Meta<typeof RegisterForm> = { component: RegisterForm, title: 'Features/RegisterForm' };
 
 export default meta;
 export const Overview = { args: {} };
